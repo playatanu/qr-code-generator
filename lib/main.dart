4f0091a1a8bd86b3fd14f7e327f9e5875c76a112
Atanu Debnath <playatanuyt@gmail.com>
2021-08-07 17:32:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bro_code/screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Bro Code"),
+          centerTitle: true,
+        ),
+        body: Home(),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,140 @@
+import 'dart:typed_data';
+import 'dart:ui';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final key = GlobalKey();
+  File? file;
+  TextEditingController _nameControler = TextEditingController();
+  String imgUrl = "";
+  String qrData = "controller";
+  String qrLable = "PLAYATANU";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            color: Colors.white,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                children: [
+                  RepaintBoundary(
+                    key: key,
+                    child: QrImage(
+                      backgroundColor: Colors.white,
+                      data: qrData,
+                      version: QrVersions.auto,
+                      size: 200.0,
+                      semanticsLabel: qrLable,
+                      // embeddedImage: AssetImage('assets/qrbg.png'),
+                      // embeddedImageStyle: QrEmbeddedImageStyle(
+                      //   size: Size(40, 40),
+                      // ),
+                    ),
+                  ),
+                  Text(
+                    "PLAYATANU",
+                    style: TextStyle(backgroundColor: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  TextField(
+                    controller: _nameControler,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a message',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            qrData = _nameControler.text;
+                          });
+                        },
+                        icon: Icon(Icons.check),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // ignore: deprecated_member_use
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    // ignore: deprecated_member_use
+                    child: FlatButton(
+                      onPressed: () async {
+                        try {
+                          RenderRepaintBoundary boundary = key.currentContext!
+                              .findRenderObject() as RenderRepaintBoundary;
+//captures qr image
+                          var image = await boundary.toImage();
+                          ByteData? byteData = await image.toByteData(
+                              format: ImageByteFormat.png);
+                          Uint8List pngBytes = byteData!.buffer.asUint8List();
+//app directory for storing images.
+                          final appDir =
+                              await getApplicationDocumentsDirectory();
+//current time
+                          var datetime = DateTime.now();
+//qr image file creation
+                          file = await File('${appDir.path}/$datetime.png')
+                              .create();
+//appending data
+                          await file?.writeAsBytes(pngBytes);
+//Shares QR image
+                          await Share.shareFiles(
+                            [file!.path],
+                            mimeTypes: ["image/png"],
+                            text: "Hello PLAYATANU",
+                          );
+                        } catch (e) {
+                          print(e.toString());
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.ios_share_outlined,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text('Share', style: TextStyle(color: Colors.blue)),
+                        ],
+                      ),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.blue,
+                              width: 2,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                  ),
+                  // ignore: deprecated_member_use
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
